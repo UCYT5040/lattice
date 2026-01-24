ee0f92fd56cfc582c6a1257e9bd86a1f2b551120
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2026-01-24 15:57:59 +0000

    Move config to server

---
+++ src/lib/server/config.ts
@@ -0,0 +1,23 @@
+import type { DatabaseProvider } from '../databaseProvider.ts';
+import type { StorageProvider } from '../storageProvider.ts';
+
+export interface LatticeConfig {
+	storageProvider: StorageProvider;
+	databaseProvider: DatabaseProvider;
+}
+
+let config: LatticeConfig;
+
+// This should be set in your SvelteKit hooks
+export function setConfig(newConfig: LatticeConfig) {
+	config = newConfig;
+}
+
+export function getConfig(): LatticeConfig {
+	if (!config) {
+		throw new Error(
+			'LatticeConfig has not been set. Please call setConfig() before accessing the configuration.'
+		);
+	}
+	return config;
+}
